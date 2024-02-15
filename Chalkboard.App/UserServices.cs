f703559af2540819a1d935ebf86cdf7dfbe7476b
EmmaDQ <sydemma@hotmail.com>
2024-02-15 11:52:06 +0100

    add userManager halfway

--- Chalkboard.App/UserServices.cs
+++ Chalkboard.App/UserServices.cs
@@ -1,16 +1,37 @@
-﻿namespace Chalkboard.App
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Chalkboard.App
 {
-    private UserManager<IdentityUser> _userManager;
-    private SignInManager<IdentityUser> _signInManager;
-    public class UserManager(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+
+    public class UserServices
     {
-        _signInManager = signInManager;
-        _userManager = userManager;
+        private UserManager<IdentityUser> _userManager;
+        private SignInManager<IdentityUser> _signInManager;
+
+        public UserServices(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+
+        public async Task<IActionResult<IdentityUser>> UserToLogIn(string username, string password)
+        {
+            IdentityUser? userToLogIn = await _userManager.FindByNameAsync(username);
+            if (userToLogIn != null)
+            {
+                var signInResult = await _signInManager.PasswordSignInAsync(userToLogIn, password, false, false);
+                if (signInResult.Succeeded)
+                {
+                    return userToLogIn;
+                }
+            }
+
+            throw new ;
+        }
+
     }
 
 
-    public async Task<IActionResult> UserToLogIn(string username)
-    {
-        return CodePagesEncodingProvider()
-    }
+
 }
