0570503d6fd720625803b4c28e0eebaee9566550
andrejakobsson7 <andre.jakobsson7@hotmail.com>
2024-02-18 22:16:09 +0100

    Implement settings page

--- ChalktBoardChat.UI/Pages/Chalkboard/Settings.cshtml.cs
+++ ChalktBoardChat.UI/Pages/Chalkboard/Settings.cshtml.cs
@@ -1,3 +1,6 @@
+using ChackBoard.Data.Repositories;
+using Chalkboard.App;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
@@ -7,37 +10,104 @@ namespace ChalktBoardChat.UI.Pages.Chalkboard
 	[BindProperties]
 	public class SettingsModel : PageModel
 	{
+
+		private readonly MessageRepository _mrepo;
+		private readonly UserManager<IdentityUser> _userManager;
+		private readonly SignInManager<IdentityUser> _signInManager;
+		public SettingsModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+		{
+			_signInManager = signInManager;
+			_userManager = userManager;
+
+		}
 		public string? OriginalUsername { get; set; }
 		public string? OriginalPassword { get; set; }
 		public string NewPassword { get; set; } = string.Empty;
 
 		[Compare(nameof(NewPassword), ErrorMessage = "Passwords doesn't match!")]
-		public string NewConfirmedPassword { get; set; } = string.Empty;
-		public string? NewUsername { get; set; }
+		public string? NewConfirmedPassword { get; set; } = string.Empty;
+		public string? NewUsername { get; set; } = string.Empty;
+		public string? UsernameErrorMessage { get; set; }
+		public List<IdentityError>? PasswordErrorMessages { get; set; } = new();
+		public string? DeleteUserErrorMessage { get; set; }
 
 		public void OnGet()
 		{
-			//Hämta info från den inloggade användaren via signedin manager och sätt original username-propertyn
 
 		}
-		public async Task OnPostUpdateUsernameAsync()
+		public async Task<IActionResult> OnPostUpdateUsernameAsync()
 		{
-			//Kommunicera med app-lagret, skicka med original username och nytt önskat username.
+			string? currentUsername = _userManager.GetUserName(User);
+			if (string.IsNullOrWhiteSpace(_userManager.GetUserName(User)))
+			{
+				UsernameErrorMessage = "Could not find a user with the current username. Please sign out and in and try again!";
+			}
+			else if (string.IsNullOrWhiteSpace(NewUsername))
+			{
+				UsernameErrorMessage = "No new username has been entered";
+			}
+			else
+			{
+				var updateUsernameResult = await new UserServices(_userManager, _signInManager).UpdateUsernameAsync(currentUsername, NewUsername);
+				if (updateUsernameResult.Errors.Any())
+				{
+					UsernameErrorMessage = updateUsernameResult.Errors.ToList()[0].Description;
+				}
+				else if (updateUsernameResult.Succeeded == false)
+				{
+					UsernameErrorMessage = "Could not find a user with the current username. Please sign out and in and try again!";
+				}
+			}
+			ResetInputFields();
+			return Page();
 		}
 
-		public async Task OnPostUpdatePasswordAsync()
+		public async Task<IActionResult> OnPostUpdatePasswordAsync()
 		{
 			//Kommunicera med app-lagret, skicka med original username. Behöver försöka logga in med hjälp av "original password". Om det går bra, ska vi i nästa steg sätta nytt lösenord.
 			//Nedanstående säkerställer att nytt lösenord och bekräftelse-lösenordet stämmer överens.
+
 			if (ModelState.IsValid)
 			{
-
+				IdentityResult updatePasswordResult = await new UserServices(_userManager, _signInManager).UpdatePasswordAsync(User.Identity.Name, OriginalPassword, NewPassword);
+				if (updatePasswordResult.Errors.Any())
+				{
+					PasswordErrorMessages = updatePasswordResult.Errors.ToList();
+				}
+				else if (updatePasswordResult.Succeeded == false)
+				{
+					PasswordErrorMessages.Add(new IdentityError()
+					{
+						Description = "Could not find a user with the current username. Please sign out and in and try again!"
+					});
+				}
 			}
+			return Page();
 		}
-		public async Task OnPostDeleteUserAsync()
+		public async Task<IActionResult> OnPostDeleteUserAsync()
 		{
 			//Kommunicera med app-lagret, skicka med original-username på den inloggade användaren (ej från inputfältet) och be om borttag.
+			IdentityResult deleteUserResult = await new UserServices(_userManager, _signInManager).DeleteUserAsync(User.Identity.Name);
+			if (deleteUserResult.Succeeded == false)
+			{
+				DeleteUserErrorMessage = "User could not be deleted. Please try again later";
+				return Page();
+			}
+			else
+			{
+				await _signInManager.SignOutAsync();
+				return RedirectToPage("/Chalkboard/Index");
+			}
 
+		}
+
+		private void ResetInputFields()
+		{
+			foreach (var input in ModelState.Values)
+			{
+				input.AttemptedValue = "";
+				input.RawValue = "";
+			}
 		}
 	}
 }
