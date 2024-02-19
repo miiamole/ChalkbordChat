0cf199792eecb26cd480a97366f076654103ab3c
miiamole <miia_pelaez@hotmail.com>
2024-02-19 09:38:33 +0100

    fixat fel på index

--- ChalktBoardChat.UI/Pages/Index.cshtml.cs
+++ ChalktBoardChat.UI/Pages/Index.cshtml.cs
@@ -5,23 +5,23 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ChalktBoardChat.UI.Pages
 {
-    public class IndexModel : PageModel
-    {
-        public UserServices Uservice;
-        public MessageServices Mservice { get; set; }
-        private IdentityUser user { get; set; }
-        private List<MessageModel> messages { get; set; }
+	public class IndexModel : PageModel
+	{
+		public UserServices Uservice;
+		public MessageServices Mservice { get; set; }
+		private IdentityUser user { get; set; }
+		private List<MessageModel> messages { get; set; }
 
-        public IndexModel(UserServices service, MessageServices mService)
-        {
-            Uservice = service;
-            Mservice = mService;
+		public IndexModel(UserServices service, MessageServices mService)
+		{
+			Uservice = service;
+			Mservice = mService;
 
-        }
+		}
 
-        public void OnGet()
-        {
+		public void OnGet()
+		{
 
-        }
-    }
+		}
+	}
 }
